e1debf71b35ba4dafd34cfd6daab4a5a62861432
Anil Shukla <40199172+anil-shukla-axway@users.noreply.github.com>
2021-01-13 17:36:10 +0530

    update(ios): updated incorrect licence

--- ios/Classes/TiNfcNativeTagTechnologyProxy.m
+++ ios/Classes/TiNfcNativeTagTechnologyProxy.m
@@ -1,9 +1,9 @@
-//
-//  TiNfcNativeTagTechnologyProxy.m
-//  ti.nfc
-//
-//  Created by Anil Shukla on 13/01/21.
-//
+/**
+ * Axway Titanium
+ * Copyright (c) 2009-present by Axway Appcelerator. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
 
 #import "TiNfcNativeTagTechnologyProxy.h"
 #import "TiBlob.h"
