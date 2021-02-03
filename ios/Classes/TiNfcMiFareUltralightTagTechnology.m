1d4e1393a003526edbfb1826146a119a771a62d3
Anil Shukla <40199172+anil-shukla-axway@users.noreply.github.com>
2021-02-03 18:04:28 +0530

    feat(ios): added the methods and properties for mifare tags

--- ios/Classes/TiNfcMiFareUltralightTagTechnology.m
+++ ios/Classes/TiNfcMiFareUltralightTagTechnology.m
@@ -9,4 +9,79 @@
 
 @implementation TiNfcMiFareUltralightTagTechnology
 
+- (NSNumber *)mifareFamily
+{
+  return [self.tag asNFCMiFareTag];
+}
+
+- (TiBuffer *)identifier
+{
+  TiBuffer *identifier = [[TiBuffer alloc] _initWithPageContext:[self pageContext]];
+  NSMutableData *data = [NSMutableData dataWithData:[[self.tag asNFCMiFareTag] identifier]];
+  [identifier setData:data];
+  return identifier;
+}
+
+- (TiBuffer *)historicalBytes
+{
+  TiBuffer *historicalBytes = [[TiBuffer alloc] _initWithPageContext:[self pageContext]];
+  NSMutableData *data = [NSMutableData dataWithData:[[self.tag asNFCMiFareTag] historicalBytes]];
+  [historicalBytes setData:data];
+  return historicalBytes;
+}
+
+- (void)sendMiFareCommand:(id)args
+{
+  TiBuffer *data = [[TiBuffer alloc] _initWithPageContext:[self pageContext]];
+  data = [[args objectAtIndex:0] valueForKey:@"data"];
+  NSMutableData *dataWith = [NSMutableData dataWithData:data];
+  NSData *dataValue = [NSData dataWithData:dataWith];
+
+  [[self.tag asNFCMiFareTag] sendMiFareCommand:data
+                             completionHandler:^(NSData *response, NSError *error) {
+                               TiBuffer *responseData = [[TiBuffer alloc] _initWithPageContext:[self pageContext]];
+                               NSMutableData *responsevalue = [NSMutableData dataWithData:response];
+                               [responseData setData:responsevalue];
+
+                               if (error == nil) {
+                                 if (![self _hasListeners:@"didSendMiFareCommand"]) {
+                                   return;
+                                 }
+                                 [self fireEvent:@"didSendMiFareCommand"
+                                      withObject:@{
+                                        @"errorCode" : NUMINTEGER([error code]),
+                                        @"errorDescription" : [error localizedDescription],
+                                        @"errorDomain" : [error domain],
+                                        @"response" : responseData
+                                      }];
+                               }
+                             }];
+}
+
+- (void)sendMiFareISO7816Command:(id)args
+{
+  TiBuffer *data = [[TiBuffer alloc] _initWithPageContext:[self pageContext]];
+  data = [[args objectAtIndex:0] valueForKey:@"apdu"];
+  NSMutableData *dataWith = [NSMutableData dataWithData:data];
+  NFCISO7816APDU *apdu = [NSData dataWithData:dataWith];
+
+  [[self.tag asNFCMiFareTag] sendMiFareISO7816Command:apdu
+                                    completionHandler:^(NSData *responseData, uint8_t sw1, uint8_t sw2, NSError *error) {
+                                      TiBuffer *responseDataValue = [[TiBuffer alloc] _initWithPageContext:[self pageContext]];
+                                      NSMutableData *responsevalue = [NSMutableData dataWithData:responseData];
+                                      [responseDataValue setData:responsevalue];
+                                      if (error == nil) {
+                                        if (![self _hasListeners:@"didSendMiFareISO7816Command"]) {
+                                          return;
+                                        }
+                                        [self fireEvent:@"didSendMiFareISO7816Command"
+                                             withObject:@{
+                                               @"errorCode" : NUMINTEGER([error code]),
+                                               @"errorDescription" : [error localizedDescription],
+                                               @"errorDomain" : [error domain],
+                                               @"responseData" : responseDataValue
+                                             }];
+                                      }
+                                    }];
+}
 @end
